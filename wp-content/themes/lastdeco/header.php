1473c43652cf6d8041e2b091b6b6f33b7987e735
Fwedede <clementfj@gmail.com>
2015-07-02 18:11:51 +0200

    Modification du theme de Lastdeco etajout d'un plugin facebook

---
+++ wp-content/themes/lastdeco/header.php
@@ -0,0 +1,32 @@
+<?php
+/**
+* The Header for our theme.
+*
+* Displays all of the <head> section and everything up till <div id="content">
+*
+* @package Illustratr
+*/
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<link href="http://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet" type="text/css">
+	<style type="text/css" media="screen">
+		@import url(<?php bloginfo('stylesheet_url'); ?>);
+	</style>
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+	<div id="page" class="hfeed site">
+		<header id="masthead" class="site-header" role="banner">
+		</header><!-- #masthead -->
+
+		<div id="spinner"></div>
+
+		<div id="content" class="site-content">
